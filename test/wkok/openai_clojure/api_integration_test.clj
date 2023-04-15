04e27d8630940ca6edbac4b18b6f0ab663166109
Werner Kok <wernerkok@gmail.com>
2023-04-15 21:57:13 +0000

    restore integration tests

--- test/wkok/openai_clojure/api_integration_test.clj
+++ test/wkok/openai_clojure/api_integration_test.clj
@@ -1,17 +1,17 @@
 (ns wkok.openai-clojure.api-integration-test
   (:require [clojure.test :refer [deftest is testing]]
-            #_[wkok.openai-clojure.api :as api]))
+            [wkok.openai-clojure.api :as api]))
 
 (deftest list-models
   (testing "lists the davinci model"
-    (is true #_(= "davinci"
+    (is (= "davinci"
            (->> (api/list-models)
                 :data
                 (some #(when (= "davinci" (:id %)) (:id %))))))))
 
 (deftest create-completion
   (testing "creates a simple completion"
-    (is true #_(contains? (api/create-completion {:model "text-davinci-003"
+    (is (contains? (api/create-completion {:model "text-davinci-003"
                                            :prompt "Say this is a test"
                                            :max_tokens 7
                                            :temperature 0})
@@ -19,7 +19,7 @@
 
 (deftest create-chat-completion
   (testing "creates a simple chat completion"
-    (is true #_(contains? (api/create-chat-completion {:model "gpt-3.5-turbo"
+    (is (contains? (api/create-chat-completion {:model "gpt-3.5-turbo"
                                                 :messages [{:role "system" :content "You are a helpful assistant."}
                                                            {:role "user" :content "Who won the world series in 2020?"}
                                                            {:role "assistant" :content "The Los Angeles Dodgers won the World Series in 2020."}
