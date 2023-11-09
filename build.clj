7d3ff8d1eaf260967bd7e7b663447c96de104d24
Werner Kok <wernerkok@gmail.com>
2023-11-09 15:19:59 +0000

    Prepare release 0.12.1

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [org.corfield.build :as bb]))
 
 (def lib 'net.clojars.wkok/openai-clojure)
-(def version "0.12.0")
+(def version "0.12.1")
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
