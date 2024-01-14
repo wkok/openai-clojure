40b6e4ca66055f980188affe446863e27c4e1b40
Werner Kok <wernerkok@gmail.com>
2024-01-14 19:28:34 +0000

    Fix tests

--- test/wkok/openai_clojure/api_integration_test.clj
+++ test/wkok/openai_clojure/api_integration_test.clj
@@ -2,21 +2,6 @@
   (:require [clojure.test :refer [deftest is testing]]
             [wkok.openai-clojure.api :as api]))
 
-(deftest list-models
-  (testing "lists the davinci model"
-    (is (= "davinci"
-           (->> (api/list-models)
-                :data
-                (some #(when (= "davinci" (:id %)) (:id %))))))))
-
-(deftest create-completion
-  (testing "creates a simple completion"
-    (is (contains? (api/create-completion {:model "text-davinci-003"
-                                           :prompt "Say this is a test"
-                                           :max_tokens 7
-                                           :temperature 0})
-                   :choices))))
-
 (deftest create-chat-completion
   (testing "creates a simple chat completion"
     (is (contains? (api/create-chat-completion {:model "gpt-3.5-turbo"
